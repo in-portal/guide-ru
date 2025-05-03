1cfaea3b703ed8fce3271fb16316df22015ce34e
Alex <aik099@users.noreply.github.com>
2025-05-03 14:15:16 +0300

    Enhance "config-property" directive with an "rst_prefix" parameter

--- docs/_ext/config_property.py
+++ docs/_ext/config_property.py
@@ -3,31 +3,36 @@ from docutils.parsers.rst import Directive, directives
 
 class ConfigProperty(Directive):
     """
-    Этот шаблон создан для того, чтобы его можно было использовать при создании специальным
-    образом оформленных списков переменных. Такие переменные так же будут якорями в той статье,
-    где они описаны.
+    This template is created to be used for creating specially formatted lists
+    of variables. Such variables will also serve as anchors in the article
+    where they are described.
     """
     has_content = False
     required_arguments = 0
     option_spec = {
         'name': directives.unchanged_required,
         'type': directives.unchanged,
+        'ref_prefix': directives.unchanged,
     }
 
     def run(self):
-        name = self.options.get('name') # название переменной
-        type = self.options.get('type') # ип входного значения (напр. int, string, bool)
+        name = self.options.get('name') # variable name (e.g., Require_SSL)
+        type = self.options.get('type') # data type (e.g., int, string, bool)
+        ref_prefix = self.options.get('ref_prefix') # prefix for referencing this link on another page (e.g., cfg_)
 
         ret = []
 
-        # Create anchor for direct access from the URL via "page_url#name".
-        target_id = name.lower()
-        target = nodes.target('', '', ids=[target_id])
-        ret.append(target)
+        if ref_prefix is not None:
+            # Create anchor for direct access from the URL via "page_url#name".
+            target_id = name.lower()
+            target = nodes.target('', '', ids=[target_id])
+            ret.append(target)
 
-        # Register the label for referencing via ":ref:`name`".
-        env = self.state.document.settings.env
-        env.domaindata['std']['labels'][target_id] = (env.docname, target_id, name)
+            # Register the label for referencing via ":ref:`name`".
+            env = self.state.document.settings.env
+
+            label_id = ref_prefix.lower() + target_id
+            env.domaindata['std']['labels'][label_id] = (env.docname, target_id, name)
 
         # Create text to show.
         name_node = nodes.strong(text=name)
--- docs/database/table_structure/portal_group.rst
+++ docs/database/table_structure/portal_group.rst
@@ -4,4 +4,9 @@ PortalGroup
 
 ...
 
+.. config-property::
+   :name: FrontRegistration
+   :type: int
+   :ref_prefix: tc_PortalGroup_
+
 .. _Data Source: http://guide.in-portal.org/rus/index.php/K4:PortalGroup
